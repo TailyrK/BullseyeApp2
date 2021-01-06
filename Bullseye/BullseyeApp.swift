f8ffbb2f9b2b8ee6d3a098a368b7fd0eb5a13669
a.m.student <tailyrdebose2017@gmail.com>
2021-01-06 08:02:25 -0600

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by A.M. Student on 1/6/21.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
