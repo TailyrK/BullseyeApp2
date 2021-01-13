deaace404532e5dae901e1d630881eff7a1d5115
a.m.student <tailyrdebose2017@gmail.com>
2021-01-13 10:48:27 -0600

    extract 8:53

--- Bullseye/Views/TextViews.swift
+++ Bullseye/Views/TextViews.swift
@@ -8,13 +8,37 @@
 import SwiftUI
 
 struct InstructionText: View {
+    var text: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(text.uppercased())
+            .bold()
+            .kerning(2.0)
+            .multilineTextAlignment(.center)
+            .lineSpacing(4.0)
+            .font(.footnote)
+            .foregroundColor(Color("TextView"))
     }
 }
 
+struct BigNumberText: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+            .kerning(-1.0)
+            .font(.largeTitle)
+            .fontWeight(.black)
+            .foregroundColor(Color("TextView"))
+    }
+}
+
+
 struct TextViews_Previews: PreviewProvider {
     static var previews: some View {
-        InstructionText()
+        VStack {
+        InstructionText(text: "Instruction")
+        BigNumberText(text: "999")
+        }
     }
 }
